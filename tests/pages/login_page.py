ac17aa25de79bba2fc675fa44eb4326b020f43c1
emanuelrestrepo22 <emadavresgar@gmail.com>
2025-02-25 13:45:32 -0300

    added url in login_page.py as POM, and other .env, changes that issuas accord ing our change

--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -1,14 +1,25 @@
+import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from tests.utils.locators import Locators  # Importamos Locators
+from selenium.webdriver.support import expected_conditions as EC
+from dotenv import load_dotenv
 
+load_dotenv() #got aour .env vars
 class LoginPage:
     """
-    Clase que representa la página de Login usando el patrón POM.
+    Class that represents login using POM.
     """
     def __init__(self, driver: WebDriver, locator: Locators):
-        self.web = driver  # ✅ Guarda el WebDriver correctamente
-        self.get = locator  # ✅ Usa correctamente el `locator`
-
+        self.web = driver  # saved webdriver successfully
+        self.get = locator  # using right 'locator'
+        self.url = os.getenv('LOGIN_URL', 'https://www.saucedemo.com/')
+        
+    def go_to_login_page(self):
+        self.web.get(self.url)
+    
     def enterUsername(self, username: str):
         """Ingresa el nombre de usuario."""
         self.get.byDataTest('username').send_keys(username)
